62a29568bb73fc6a4d06f76a9c919cb5cc76aab2
taras <tarik2002sta@gmail/com>
2022-12-08 11:56:49 +0200

    firct commit

---
+++ script.js
@@ -0,0 +1,33 @@
+
+
+
+
+
+
+
+
+const firstWord = prompt('First word');
+const secondWord = prompt(' Second word');
+const thirdWord = prompt('Third word');
+
+const firstConversion = String(firstWord);
+const secondConversion = String(secondWord);
+const thirdConversion = String(thirdWord);
+
+const resultString = firstConversion + ', ' + secondConversion + ', ' + thirdConversion + '.';
+
+alert(resultString);
+
+const firstNumber = prompt('Number #1');
+const secondNumber = prompt('Number #2');
+const thirdNumber = prompt('Number #3');
+const fourthNumber = prompt('Number #4');
+const fifthNumber = prompt('Number #5');
+
+const firstnumConversion = Number(firstNumber);
+const secondnumConversion = Number(secondNumber);
+const thirdnumConversion = Number(thirdNumber);
+const fourthnumConversion = Number(fourthNumber);
+const fifthnumConversion = Number(fifthNumber);
+
+alert(firstnumConversion + ' ' + secondnumConversion + ' ' + thirdnumConversion + ' ' + fourthnumConversion + ' ' + fifthnumConversion);
